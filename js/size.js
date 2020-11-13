a1eb3510796f642c803b85ac4e3bd4719cef11d7
Kolbak <kushaevmarat@mail.ru>
2020-11-13 22:43:11 +0300

    d3 >> p5

--- js/size.js
+++ js/size.js
@@ -1,31 +1,31 @@
 if (typeof size === "undefined") size = {};
 
-function axes(width, height, margin) {
-  two_third = (margin * 2) / 3;
-  svg
-    .append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.svg.axis().scale(x).orient("bottom"));
-  svg
-    .append("text")
-    .attr("class", "x-label")
-    .attr(
-      "transform",
-      "translate(" + width / 2 + "," + (height + two_third) + ")"
-    )
-    .text("X");
+// function axes(width, height, margin) {
+//   two_third = (margin * 2) / 3;
+//   svg
+//     .append("g")
+//     .attr("class", "x axis")
+//     .attr("transform", "translate(0," + height + ")")
+//     .call(d3.svg.axis().scale(x).orient("bottom"));
+//   svg
+//     .append("text")
+//     .attr("class", "x-label")
+//     .attr(
+//       "transform",
+//       "translate(" + width / 2 + "," + (height + two_third) + ")"
+//     )
+//     .text("X");
 
-  svg
-    .append("g")
-    .attr("class", "y axis")
-    .call(d3.svg.axis().scale(y).orient("left"));
-  svg
-    .append("text")
-    .attr("class", "y-label")
-    .attr("transform", "translate(" + -two_third + "," + height / 2 + ")")
-    .text("Y");
-}
+//   svg
+//     .append("g")
+//     .attr("class", "y axis")
+//     .call(d3.svg.axis().scale(y).orient("left"));
+//   svg
+//     .append("text")
+//     .attr("class", "y-label")
+//     .attr("transform", "translate(" + -two_third + "," + height / 2 + ")")
+//     .text("Y");
+// }
 
 points = [];
 let purpleMain = {
@@ -37,7 +37,7 @@ let purpleMain = {
     y: 150,
   };
 function genPoints(width, height, quantity, rad) {
-  greenMain.x  = width / 2 + rad[0] - 5;
+  greenMain.x = width / 2 + rad[0] - 5;
   purpleMain.x = width / 2 - rad[1] + 5;
 
   function dist(p1, p2) {
@@ -54,7 +54,7 @@ function genPoints(width, height, quantity, rad) {
 
     if (dot.cls && dist(dot, purpleMain) <= rad[1]) {
       points.push(dot);
-    } else if(!dot.cls && dist(dot, greenMain) <= rad[0]) {
+    } else if (!dot.cls && dist(dot, greenMain) <= rad[0]) {
       points.push(dot);
     } else {
       i--;
@@ -62,35 +62,36 @@ function genPoints(width, height, quantity, rad) {
   }
 }
 
-function drawDots() {
-  for (let i = 0, l = points.length; i < l; i++) {
-    drawPoint(points[i]);
-  }
-}
+// function drawDots() {
+//   for (let i = 0, l = points.length; i < l; i++) {
+//     drawPoint(points[i]);
+//   }
+// }
 
 // x, y -- coords; cls -- class
-function drawPoint({ x, y, r, cls }) {
-  let fill = cls ? "purple" : "green";
-  svg
-    .append("circle")
-    .attr("class", `cls-${cls}`)
-    .attr("cx", x)
-    .attr("cy", y)
-    .attr("r", r)
-    .attr("fill", fill);
-}
+// function drawPoint({ x, y, r, cls }) {
+//   let fill = cls ? "purple" : "green";
+//   svg
+//     .append("circle")
+//     .attr("class", `cls-${cls}`)
+//     .attr("cx", x)
+//     .attr("cy", y)
+//     .attr("r", r)
+//     .attr("fill", fill);
+// }
 
-function drawSizeSep({ x, y, len }) {
-  let fill = "purple";
-  svg
-    .append("line")
-    .attr("class", "sep")
-    .attr("x1", x)
-    .attr("y1", y)
-    .attr("x2", x)
-    .attr("y2", y + len)
-    .style("stroke", fill);
-}
+// function drawSizeSep(w, h) {
+// line(w / 2, 0, w / 2, h);
+// let fill = "purple";
+// svg
+//   .append("line")
+//   .attr("class", "sep")
+//   .attr("x1", x)
+//   .attr("y1", y)
+//   .attr("x2", x)
+//   .attr("y2", y + len)
+//   .style("stroke", fill);
+// }
 
 function addSlider(min, max, value, step) {
   let label = container.append("div").append("label");
@@ -120,19 +121,19 @@ function sizeProportions() {
   //     .range([0, w])
   //     .clamp(true);
   let slider = d3.select(".size-ratio")[0][0];
-  let scale = d3.scale
-    .linear()
-    .domain([+slider.min, +slider.max])
-    .range([0, maxRad])
-    .clamp(true);
+  // let scale = d3.scale
+  //   .linear()
+  //   .domain([+slider.min, +slider.max])
+  //   .range([0, maxRad])
+  //   .clamp(true);
 
   let percPurple = Math.round((+slider.value / +slider.max) * 100);
-  let percGreen  = 100 - percPurple;
-  let percRad    = maxRad / 100;
+  let percGreen = 100 - percPurple;
+  let percRad = maxRad / 100;
   // console.log('perc :>> ', percPurple, percGreen);
 
   genPoints(gWidth, gHeight, 60, [percRad * percGreen, percRad * percPurple]);
-  drawDots();
+  // drawDots();
 }
 // для третьего графика
 // function connectSliderSep(w) {
@@ -149,34 +150,61 @@ function sizeProportions() {
 //   .attr("x1", `${xScale(slider.value)}px`)
 //   .attr("x2", `${xScale(slider.value)}px`);
 // }
-
-let container, svg, x, y, line, gWidth, gHeight, maxRad;
-size.init = function (id, width, height, margin) {
-  gWidth = width;
-  gHeight = height;
-
-  container = d3.select("#" + id);
-  (x = d3.scale.linear().domain([0, 1]).range([0, width])),
-    (y = d3.scale.linear().domain([0, 1]).range([height, 0]));
-
-  svg = container
-    .append("svg")
-    .attr("width", width + 2 * margin)
-    .attr("height", height + 2 * margin)
-    .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")");
-
-  let sizeSepOptions = {
-    len: height,
-    x: width / 2,
-    y: 0,
-  };
-
+let gWidth = 400,
+  gHeight = 300,
   maxRad = 100;
+function setup() {
+  genPoints(gWidth, gHeight, 150, [maxRad, maxRad]);
+  let cnv = createCanvas(gWidth, gHeight);
+  cnv.parent("chart-cont");
+  // select("#chart-cont");
+  createElement("input")
+  .parent('#chart-cont');
+}
 
-  axes(width, height, margin);
-  genPoints(width, height, 150, [maxRad, maxRad]);
-  drawDots();
-  drawSizeSep(sizeSepOptions);
-  addSlider(0, 1, 0.5, 50, width);
-};
+function draw() {
+  background(230);
+
+  //POINTS
+  for (let i = 0, l = points.length; i < l; i++) {
+    noStroke();
+    points[i].cls ? fill(99, 64, 156) : fill(20, 120, 20);
+    circle(points[i].x, points[i].y, points[i].r);
+  }
+
+  // SEPARATOR
+  stroke("purple");
+  line(gWidth / 2, 0, gWidth / 2, gHeight);
+  // drawSizeSep(gWidth, gHeight);
+}
+// size.init = function (id, width, height, margin) {
+// gWidth = width;
+// gHeight = height;
+
+// container = d3.select("#" + id);
+// (x = d3.scale.linear().domain([0, 1]).range([0, width])),
+//   (y = d3.scale.linear().domain([0, 1]).range([height, 0]));
+
+// svg = container
+//   .append("svg")
+//   .attr("width", width + 2 * margin)
+//   .attr("height", height + 2 * margin)
+//   .append("g")
+//   .attr("transform", "translate(" + margin + "," + margin + ")");
+
+// let sizeSepOptions = {
+//   len: height,
+//   x: width / 2,
+//   y: 0,
+// };
+
+// maxRad = 100;
+
+// axes(width, height, margin);
+// genPoints(width, height, 150, [maxRad, maxRad]);
+// drawDots();
+// setup();
+// draw();
+// drawSizeSep(sizeSepOptions);
+// addSlider(0, 1, 0.5, 50, width);
+// };
