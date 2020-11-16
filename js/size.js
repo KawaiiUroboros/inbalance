3e7b9bb7d34b4bf5c9548c5823e4d2a9b6bca112
Kolbak <kushaevmarat@mail.ru>
2020-11-16 22:37:55 +0300

    fix marat

--- js/size.js
+++ js/size.js
@@ -31,8 +31,8 @@ points = [];
 let purpleMain = { },
   greenMain    = { };
 function genPoints(width, height, quantity, rad) {
-  greenMain.x = width / 2 + rad[0] / 2;
-  purpleMain.x = width / 2 - rad[1] / 2;
+  greenMain.x = width / 2 + rad[0] - 5;
+  purpleMain.x = width / 2 - rad[1] + 5;
 
   function dist(p1, p2) {
     return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
